0d1ce3df04f665809c8110a0a7afa4a3d6ad1f33
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-08 14:31:56 -0700

    prediction works

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -10,16 +10,18 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
+mult = ones(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+theta(1) = 0
 
-J = 1/m*sum( -y .* log( sigmoid( X*theta )) - (1 .- y) .* log(1 - sigmoid( X*theta ))) + lambda/(2*m) * sum(theta().^2)
+J = 1/m*sum( -y .* log( sigmoid( X*theta )) - (1 .- y) .* log(1 - sigmoid( X*theta ))) + lambda/(2*m) * sum(theta .^2)
 
-grad =  1/m * sum( (sigmoid( X * theta ) - y ) .* X ) - lambda/m .* theta
+grad =  1/m * sum( (sigmoid( X * theta ) - y ) .* X )
 
 % =============================================================
 
